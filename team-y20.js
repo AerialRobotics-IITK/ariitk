302842d15cc2f3a40d06ba26592c28b58e4b8cee
vakshit <akshitv18@gmail.com>
2021-12-02 02:16:52 +0530

    add y20

---
+++ team-y20.js
@@ -0,0 +1,210 @@
+const teamInfo = [
+  {
+    name: "Sujal Harkut",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1YrviSnsFt4OEks8BU8PrHKLR9p11pTdJ/preview",
+    fb: "https://www.facebook.com/sujalharkut",
+    insta: "https://www.instagram.com/sujal_harkut/",
+    linkedin: "https://www.linkedin.com/in/sujal-harkut-8027601b9/",
+    github: "https://github.com/sujalharkut",
+  },
+  {
+    name: "Dev Barbhaya",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1rdx6-wvqzKVIAbEmLtH2Lmc2i_Ui5_iS/preview",
+    fb: "https://www.facebook.com/dev.barbhaya.18/",
+    insta: "https://www.instagram.com/devbarbedwire/",
+    linkedin: "https://www.linkedin.com/in/dev-barbhaya-32a09b210/",
+    github: "https://github.com/xDB-9",
+  },
+  {
+    name: "Pratyush Gupta",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1__M-R2lubG54OKCB51NeDQe18OXegLIM/preview",
+    fb: "https://www.facebook.com/pratyush.gupta.39566/",
+    insta: "https://www.instagram.com/pratyush.gupta.39566/",
+    linkedin: "https://www.linkedin.com/in/pratyush-gupta-60858521b/",
+    github: "https://github.com/PratyushGupta0",
+  },
+  {
+    name: "Jaya Gupta",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=15GxtRRq3NIA_dLwI-EtzNlJjmds97qz3/preview",
+    fb: "https://www.facebook.com/27JayaGupta",
+    insta: "https://www.instagram.com/jayagupta_27/",
+    linkedin: "https://www.linkedin.com/in/jaya-gupta-78a7b7205/",
+    github: "https://github.com/27-JayaGupta",
+  },
+  {
+    name: "Aryan Jain",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1AWLpQfnz9rVQh6fK0Bd8LRbJRmIkdEY0/preview",
+    fb: "https://www.facebook.com/profile.php?id=100056025635847",
+    insta: "https://www.instagram.com/jain_aryan_19/",
+    linkedin: "https://www.linkedin.com/in/aryan-jain-1908/",
+    github: "https://github.com/aryanjain19",
+  },
+  {
+    name: "Saransh Shivhare",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=13zAhNK-TD2extQhdsxyfN_SbPwV5cHUo/preview",
+    fb: "https://www.facebook.com/profile.php?id=100006630047839",
+    insta: "https://www.instagram.com/saransh_shivhare/",
+    linkedin: "https://www.linkedin.com/in/saransh-shivhare-4b02ba200/",
+    github: "https://github.com/saranshg20",
+  },
+  {
+    name: "Arth Banka",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1gaeBl3Z-5J7BoiIxJ10ho4emRZYFmUhm/preview",
+    fb: "https://www.facebook.com/arth.banka/",
+    insta: "https://www.instagram.com/arth_banka/",
+    linkedin: "https://www.linkedin.com/in/arth-b-6a083a125/",
+    github: "https://github.com/Arth-DEVOPS",
+  },
+  {
+    name: "Rahul Rustagi",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1zNRNrS9weGmQd5ehODrEezlGpU8ylwlo/preview",
+    fb: "https://www.facebook.com/profile.php?id=100002427998692",
+    insta: "https://www.instagram.com/me_be_rahul/",
+    linkedin: "https://www.linkedin.com/in/rahul-rustagi-34254321b/",
+    github: "https://github.com/rrustagi20",
+  },
+  {
+    name: "Adit Jain",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1_4BT9BzIcSLDCVYn4YxY8kS4U7EOkSjo/preview",
+    fb: "https://www.facebook.com/adit.jain.1675/",
+    insta: "https://www.instagram.com/jadit_19/",
+    linkedin: "https://www.linkedin.com/in/adit-jain-658481b9/",
+    github: "https://github.com/Jadit19",
+  },
+  {
+    name: "Rajarshi Dutta",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1PNGZ4DeiVqO6VPuaZQwipgRIvk5pDGpm/preview",
+    fb: "https://www.facebook.com/rajarshi.dutta.507",
+    insta: "https://www.instagram.com/rajarshi.dutta.507/?hl=en",
+    linkedin: "https://www.linkedin.com/in/rajarshi-dutta-1074941ba/",
+    github: "https://github.com/Rajarshi1001",
+  },
+  {
+    name: "Sharath Kumar V",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1KKbbFUEqE70dqFx-t8D54e1l7ns47PCj/preview",
+    fb: "https://www.facebook.com/sharath.kumarv.39/",
+    insta: "https://www.instagram.com/sharath_0313/",
+    linkedin: "https://www.linkedin.com/in/sharath-kumar-v-bb907421b/",
+    github: "https://github.com/Sharath0313",
+  },
+  {
+    name: "B.Anshuman",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1eqt2_XNJm5h0ocQC8X4_wl7tPtpSVb1u/preview",
+    fb: "https://www.facebook.com/b.anshuman1305",
+    insta: "https://www.instagram.com/human_b.a/",
+    linkedin: "https://www.linkedin.com/in/b-anshuman13/",
+    github: "https://github.com/ba-13",
+  },
+  {
+    name: "Subhrajit Mishra",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=11hl_wdN1knF6qX8O3bQyg2HfrB_dJMtq/preview",
+    fb: "https://www.facebook.com/subhrajit.mishra.37017",
+    insta: "https://www.instagram.com/subhrajit.mishra.101/",
+    linkedin: "http://www.linkedin.com/in/subhrajit-mishra-a0b851210",
+    github: "https://github.com/subhrajitm20",
+  },
+  {
+    name: "Ujwal Jyot Panda",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1uldEaiFgD4v9pceKbbvihcDa2aoS7zCK/preview",
+    fb: "https://www.facebook.com/ujwaljyot.panda.9/",
+    insta: "https://www.instagram.com/ujwal.j.p/",
+    linkedin: "NA",
+    github: "https://github.com/ujwaljp",
+  },
+  {
+    name: "Aditya Anand",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1vQxYXNDecI9KEW6fJi-RacRBGQBQ8Muy/preview",
+    fb: "https://www.facebook.com/adityaanandaa28",
+    insta: "https://www.instagram.com/adityaanand28/",
+    linkedin: "https://www.linkedin.com/in/adityaanand28",
+    github: "https://github.com/adityaanand28",
+  },
+  {
+    name: "Atharva Dehadraya",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1J225F5Nv4Qae0RZOo-gGhfXACMfY1A2_/preview",
+    fb: "https://www.facebook.com/atharva.dehadraya",
+    insta: "NA",
+    linkedin: "https://www.linkedin.com/in/atharvadehadraya",
+    github: "https://github.com/QuantuMAtharva",
+  },
+  {
+    name: "Batta Soumith",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1ERS7nwL-12_jEbaAg0mhN3JW5KBdAmJf/preview",
+    fb: "https://www.facebook.com/soumith.monu",
+    insta: "https://www.instagram.com/s0umith/",
+    linkedin: "https://www.linkedin.com/in/soumith-chowdary",
+    github: "https://github.com/s0umith",
+  },
+  {
+    name: "Rakshit Yash ",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1ES5RpMYZ__jCzkt0OMQSC7KNbf3iQm9X/preview",
+    fb: "https://www.facebook.com/rakshit.yash.142",
+    insta: "https://www.instagram.com/rxk_shit/",
+    linkedin: "https://www.linkedin.com/in/rakshit-yash-83a45b214/",
+    github: "https://github.com/RakshitYash",
+  },
+  {
+    name: "L Gokulnath",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1vo5mZtGBRjBkpMw-qrjsvBz8i8rx0KLN/preview",
+    fb: "https://www.facebook.com/gokulnath.l.75/",
+    insta: "NA",
+    linkedin: "NA",
+    github: "https://github.com/lgokulnath",
+  },
+  {
+    name: "Aryaman badkul",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1uOEEOwzM-KHXXNBAZk8F2cEBwA0yV8Y4/preview",
+    fb: "https://www.facebook.com/aryaman.badkul/",
+    insta: "https://www.instagram.com/r_a_mann/",
+    linkedin: "https://www.linkedin.com/in/aryaman-badkul-755373204/",
+    github: "https://github.com/aryaman-a-student",
+  },
+  {
+    name: "Karan Jeyasankar",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=1ZCzHcptFf7C-6JM9H-83lobmxR_AdAng/preview",
+    fb: "https://www.facebook.com/karan.jey.37/",
+    insta: "karan_jey_",
+    linkedin: "https://www.linkedin.com/in/karan-jeyasankar-98135595/",
+    github: "karan-jey",
+  },
+  {
+    name: "Aryan Sharma",
+    image:
+      "https://drive.google.com/u/0/open?usp=forms_web&id=12lTqwgNOOBvr8XG9I1qz_eXscefTI4-8/preview",
+    fb: "https://www.facebook.com/profile.php?id=100056630960019",
+    insta: "https://www.instagram.com/aryan_shrama123",
+    linkedin: "www.linkedin.com/in/aryan-sharma-540338215",
+    github: "https://github.com/aryans-20",
+  },
+];
+
+const element = document.getElementById("y20");
+
+teamInfo.forEach((member) => {
+  const div = document.createElement("div");
+  div.classList.add("col-md-6");
+  div.classList.add("col-lg-4");
+  div.innerHTML = `<div class=\"team-minimal\"><div class=\"team-minimal-figure\"><img src=\"images/team/y20/${member.name}.jpg" alt=\"\" width=\"370\" height=\"395\" /><ul class=\"team-minimal-soc-list\"><li><a class=\"icon icon-lg fa-facebook\" href=\"${member.fb}"></a></li><li><a class=\"icon icon-lg fa-instagram\" href=\"${member.insta}\"></a></li><li><a class=\"icon icon-lg fa-linkedin\" href=\"${member.linkedin}\"></a></li></ul></div><div class=\"team-minimal-caption\"><h4><a class=\"team-name\"><spanstyle=\"font-family:Rajdhani;letter-spacing:-34;font-weight:bolder;\">${member.name}</span></a></h4><p style=\"font-family:'SF UI Display Light';\">Member</p></div></div>`;
+  element.appendChild(div);
+});
