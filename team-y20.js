c69bd16a2f3e2ada67d5d1bfe49b205b6c69c5b7
vakshit <akshitv18@gmail.com>
2021-12-12 12:28:49 +0530

    update image

--- team-y20.js
+++ team-y20.js
@@ -1,9 +1,7 @@
 const teamInfo = [
   {
     name: "Akshit Verma",
-    role: "Junior Member & Creative Member",
-    image:
-      "https://drive.google.com/file/d/1BUYjHQ8PucCFuucpzHEcxMx_FBZwQAnM/view?usp=sharing/preview",
+    role: "Junior & Creative Member",
     fb: "https://www.facebook.com/profile.php?id=100057273051263",
     insta: "https://www.instagram.com/ronnie.3784/",
     linkedin: "https://www.linkedin.com/in/akshit-verma-205aa5219/",
@@ -12,8 +10,6 @@ const teamInfo = [
   {
     name: "B.Anshuman",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1eqt2_XNJm5h0ocQC8X4_wl7tPtpSVb1u/preview",
     fb: "https://www.facebook.com/b.anshuman1305",
     insta: "https://www.instagram.com/human_b.a/",
     linkedin: "https://www.linkedin.com/in/b-anshuman13/",
@@ -22,8 +18,6 @@ const teamInfo = [
   {
     name: "Pratyush Gupta",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1__M-R2lubG54OKCB51NeDQe18OXegLIM/preview",
     fb: "https://www.facebook.com/pratyush.gupta.39566/",
     insta: "https://www.instagram.com/pratyush.gupta.39566/",
     linkedin: "https://www.linkedin.com/in/pratyush-gupta-60858521b/",
@@ -33,8 +27,6 @@ const teamInfo = [
   {
     name: "Aryan Jain",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1AWLpQfnz9rVQh6fK0Bd8LRbJRmIkdEY0/preview",
     fb: "https://www.facebook.com/profile.php?id=100056025635847",
     insta: "https://www.instagram.com/jain_aryan_19/",
     linkedin: "https://www.linkedin.com/in/aryan-jain-1908/",
@@ -43,8 +35,6 @@ const teamInfo = [
   {
     name: "Saransh Shivhare",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=13zAhNK-TD2extQhdsxyfN_SbPwV5cHUo/preview",
     fb: "https://www.facebook.com/profile.php?id=100006630047839",
     insta: "https://www.instagram.com/saransh_shivhare/",
     linkedin: "https://www.linkedin.com/in/saransh-shivhare-4b02ba200/",
@@ -54,8 +44,6 @@ const teamInfo = [
   {
     name: "Rahul Rustagi",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1zNRNrS9weGmQd5ehODrEezlGpU8ylwlo/preview",
     fb: "https://www.facebook.com/profile.php?id=100002427998692",
     insta: "https://www.instagram.com/me_be_rahul/",
     linkedin: "https://www.linkedin.com/in/rahul-rustagi-34254321b/",
@@ -64,8 +52,6 @@ const teamInfo = [
   {
     name: "Adit Jain",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1_4BT9BzIcSLDCVYn4YxY8kS4U7EOkSjo/preview",
     fb: "https://www.facebook.com/adit.jain.1675/",
     insta: "https://www.instagram.com/jadit_19/",
     linkedin: "https://www.linkedin.com/in/adit-jain-658481b9/",
@@ -74,8 +60,6 @@ const teamInfo = [
   {
     name: "Rajarshi Dutta",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1PNGZ4DeiVqO6VPuaZQwipgRIvk5pDGpm/preview",
     fb: "https://www.facebook.com/rajarshi.dutta.507",
     insta: "https://www.instagram.com/rajarshi.dutta.507/?hl=en",
     linkedin: "https://www.linkedin.com/in/rajarshi-dutta-1074941ba/",
@@ -84,8 +68,6 @@ const teamInfo = [
   {
     name: "Sharath Kumar V",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1KKbbFUEqE70dqFx-t8D54e1l7ns47PCj/preview",
     fb: "https://www.facebook.com/sharath.kumarv.39/",
     insta: "https://www.instagram.com/sharath_0313/",
     linkedin: "https://www.linkedin.com/in/sharath-kumar-v-bb907421b/",
@@ -95,8 +77,6 @@ const teamInfo = [
   {
     name: "Ujwal Jyot Panda",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1uldEaiFgD4v9pceKbbvihcDa2aoS7zCK/preview",
     fb: "https://www.facebook.com/ujwaljyot.panda.9/",
     insta: "https://www.instagram.com/ujwal.j.p/",
     linkedin: "#",
@@ -105,8 +85,6 @@ const teamInfo = [
   {
     name: "Aditya Anand",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1vQxYXNDecI9KEW6fJi-RacRBGQBQ8Muy/preview",
     fb: "https://www.facebook.com/adityaanandaa28",
     insta: "https://www.instagram.com/adityaanand28/",
     linkedin: "https://www.linkedin.com/in/adityaanand28",
@@ -115,8 +93,6 @@ const teamInfo = [
   {
     name: "Atharva Dehadraya",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1J225F5Nv4Qae0RZOo-gGhfXACMfY1A2_/preview",
     fb: "https://www.facebook.com/atharva.dehadraya",
     insta: "#",
     linkedin: "https://www.linkedin.com/in/atharvadehadraya",
@@ -125,8 +101,6 @@ const teamInfo = [
   {
     name: "Batta Soumith",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1ERS7nwL-12_jEbaAg0mhN3JW5KBdAmJf/preview",
     fb: "https://www.facebook.com/soumith.monu",
     insta: "https://www.instagram.com/s0umith/",
     linkedin: "https://www.linkedin.com/in/soumith-chowdary",
@@ -135,8 +109,6 @@ const teamInfo = [
   {
     name: "Rakshit Yash ",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1ES5RpMYZ__jCzkt0OMQSC7KNbf3iQm9X/preview",
     fb: "https://www.facebook.com/rakshit.yash.142",
     insta: "https://www.instagram.com/rxk_shit/",
     linkedin: "https://www.linkedin.com/in/rakshit-yash-83a45b214/",
@@ -145,18 +117,14 @@ const teamInfo = [
   {
     name: "L Gokulnath",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1vo5mZtGBRjBkpMw-qrjsvBz8i8rx0KLN/preview",
     fb: "https://www.facebook.com/gokulnath.l.75/",
     insta: "#",
     linkedin: "#",
     github: "https://github.com/lgokulnath",
   },
   {
-    name: "Aryaman badkul",
+    name: "Aryaman Badkul",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1uOEEOwzM-KHXXNBAZk8F2cEBwA0yV8Y4/preview",
     fb: "https://www.facebook.com/aryaman.badkul/",
     insta: "https://www.instagram.com/r_a_mann/",
     linkedin: "https://www.linkedin.com/in/aryaman-badkul-755373204/",
@@ -165,8 +133,6 @@ const teamInfo = [
   {
     name: "Karan Jeyasankar",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1ZCzHcptFf7C-6JM9H-83lobmxR_AdAng/preview",
     fb: "https://www.facebook.com/karan.jey.37/",
     insta: "https://www.linkedin.com/in/karan_jey_",
     linkedin: "https://www.linkedin.com/in/karan-jeyasankar-98135595/",
@@ -175,8 +141,6 @@ const teamInfo = [
   {
     name: "Aryan Sharma",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=12lTqwgNOOBvr8XG9I1qz_eXscefTI4-8/preview",
     fb: "https://www.facebook.com/profile.php?id=100056630960019",
     insta: "https://www.instagram.com/aryan_shrama123",
     linkedin: "www.linkedin.com/in/aryan-sharma-540338215",
@@ -185,8 +149,6 @@ const teamInfo = [
   {
     name: "Sujal Harkut",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1YrviSnsFt4OEks8BU8PrHKLR9p11pTdJ/preview",
     fb: "https://www.facebook.com/sujalharkut",
     insta: "https://www.instagram.com/sujal_harkut/",
     linkedin: "https://www.linkedin.com/in/sujal-harkut-8027601b9/",
@@ -195,8 +157,6 @@ const teamInfo = [
   {
     name: "Dev Barbhaya",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1rdx6-wvqzKVIAbEmLtH2Lmc2i_Ui5_iS/preview",
     fb: "https://www.facebook.com/dev.barbhaya.18/",
     insta: "https://www.instagram.com/devbarbedwire/",
     linkedin: "https://www.linkedin.com/in/dev-barbhaya-32a09b210/",
@@ -205,8 +165,6 @@ const teamInfo = [
   {
     name: "Jaya Gupta",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=15GxtRRq3NIA_dLwI-EtzNlJjmds97qz3/preview",
     fb: "https://www.facebook.com/27JayaGupta",
     insta: "https://www.instagram.com/jayagupta_27/",
     linkedin: "https://www.linkedin.com/in/jaya-gupta-78a7b7205/",
@@ -215,8 +173,6 @@ const teamInfo = [
   {
     name: "Arth Banka",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=1gaeBl3Z-5J7BoiIxJ10ho4emRZYFmUhm/preview",
     fb: "https://www.facebook.com/arth.banka/",
     insta: "https://www.instagram.com/arth_banka/",
     linkedin: "https://www.linkedin.com/in/arth-b-6a083a125/",
@@ -225,12 +181,26 @@ const teamInfo = [
   {
     name: "Subhrajit Mishra",
     role: "Junior Member",
-    image:
-      "https://drive.google.com/u/0/open?usp=forms_web&id=11hl_wdN1knF6qX8O3bQyg2HfrB_dJMtq/preview",
     fb: "https://www.facebook.com/subhrajit.mishra.37017",
     insta: "https://www.instagram.com/subhrajit.mishra.101/",
     linkedin: "http://www.linkedin.com/in/subhrajit-mishra-a0b851210",
     github: "https://github.com/subhrajitm20",
+  },
+  {
+    name: "Gagan Sipani",
+    role: "Junior Member",
+    fb: "https://www.facebook.com/gagan.sipani.9",
+    insta: "#",
+    linkedin: "https://www.linkedin.com/in/gagan-sipani-431859153",
+    github: "https://github.com/gagans20",
+  },
+  {
+    name: "Juee",
+    role: "Junior Member",
+    fb: "#",
+    insta: "#",
+    linkedin: "#",
+    github: "#",
   },
 ];
 
@@ -240,6 +210,6 @@ teamInfo.forEach((member) => {
   const div = document.createElement("div");
   div.classList.add("col-md-6");
   div.classList.add("col-lg-4");
-  div.innerHTML = `<div class="team-minimal"><div class="team-minimal-figure"><img src="images/team/y20/${member.name}.jpg" alt="" width="370" height="395" style="width:370px; height:440px; object-fit: fill;" /><ul class="team-minimal-soc-list"><li><a class="icon icon-lg fa-facebook" href="${member.fb}"></a></li><li><a class="icon icon-lg fa-instagram" href="${member.insta}"></a></li><li><a class="icon icon-lg fa-linkedin" href="${member.linkedin}"></a></li></ul></div><div class="team-minimal-caption"><h4><a class="team-name"><spanstyle="font-family:Rajdhani;letter-spacing:-34;font-weight:bolder;">${member.name}</span></a></h4><p style="font-family:'SF UI Display Light';">${member.role}</p></div></div>`;
+  div.innerHTML = `<div class="team-minimal"><div class="team-minimal-figure"><img src="images/team/y20/${member.name}.jpg" alt="" width="370" height="395" style="width:370px; height:460px; object-fit: fill;" /><ul class="team-minimal-soc-list"><li><a class="icon icon-lg fa-facebook" href="${member.fb}"></a></li><li><a class="icon icon-lg fa-instagram" href="${member.insta}"></a></li><li><a class="icon icon-lg fa-linkedin" href="${member.linkedin}"></a></li></ul></div><div class="team-minimal-caption"><h4><a class="team-name"><spanstyle="font-family:Rajdhani;letter-spacing:-34;font-weight:bolder;">${member.name}</span></a></h4><p style="font-family:'SF UI Display Light';">${member.role}</p></div></div>`;
   element.appendChild(div);
 });
