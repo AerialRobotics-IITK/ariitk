aa0fae64c19a4ef81f5896fc2ab8a1e154325599
saatvik <33216181+saatviksingh77@users.noreply.github.com>
2025-06-23 11:25:25 +0530

    fixes

--- components/Team/ParallaxTeam.tsx
+++ components/Team/ParallaxTeam.tsx
@@ -9,37 +9,45 @@ export default function ParallaxTeam() {
     offset: ["start start", "end start"],
   });
   const backgroundY = useTransform(scrollYProgress, [0, 1], ["0%", "100%"]);
-  const textY = useTransform(scrollYProgress, [0, 1], ["-100%", "330%"]);
+  const bottomY = useTransform(scrollYProgress, [0, 1], ["0%", "30%"]);
+  const textY = useTransform(scrollYProgress, [0, 1], ["-100%", "400%"]);
+  const textScale = useTransform(scrollYProgress, [0, 1], [1, 0.8]);
 
   return (
     <div
       ref={ref}
       className="w-full h-screen overflow-hidden relative grid place-items-center"
     >
+      {/* heading */}
       <motion.h1
-        style={{ y: textY }}
+        style={{ y: textY , scale: textScale}}
         className="text-center text-9xl font-extrabold text-white relative z-10"
       >
         ARIITK 2025/26
       </motion.h1>
 
+      {/* background image */}
       <motion.div
         className="absolute inset-0 z-0"
         style={{
           backgroundImage: `url('/images/team/current/teamphoto.jpg')`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
-          y: backgroundY,
+          y: backgroundY
         }}
       />
-      <div
+      
+      {/* bottom half image */}
+      <motion.div
         className="absolute inset-0 z-20"
         style={{
           backgroundImage: `url('/images/team/current/team-bottom.png')`,
           backgroundPosition: "bottom",
           backgroundSize: "cover",
+          y: bottomY
         }}
       />
+
     </div>
   );
 }
